42d7c0d79762e56389d39707826d221142eb52d8
Georgi Donchev <georgeuk26@gmail.com>
2025-01-16 22:50:06 +0000

    Add about page

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-// app.get('/about', (req, res) => {
-    
-// });
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...')
 ); 
