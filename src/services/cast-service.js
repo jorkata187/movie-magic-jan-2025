32168f46a557f548ee590c193333321467d6b53c
Georgi Donchev <georgeuk26@gmail.com>
2025-01-25 20:13:46 +0000

    Exclude casts that are already attached

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -1,7 +1,15 @@
 import Cast from "../models/Cast.js";
+
 export default{
-    getAll() {
-        return Cast.find({});
+    getAll(filter = {}) {
+        let query = Cast.find({});
+
+        if (filter.exclude) {
+            query = query.nin('_id', filter.exclude);
+    // query = query.find({_id: {$nin: filter.exclude}});
+
+        }
+        return query;
     },
     create(castData) {
        return Cast.create(castData);
