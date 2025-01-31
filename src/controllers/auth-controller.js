e0017ed5547aeab3a44ce34693b9c0f9aeca1166
Georgi Donchev <georgeuk26@gmail.com>
2025-01-31 19:24:35 +0000

    Send cookie-token to client on login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -24,12 +24,12 @@ authController.post('/login', async (req, res) => {
 
     try {
         const token = await authService.login(email, password); 
+        res.cookie('auth', token);
+        res.redirect('/');
     } catch (err) {
         console.log(err.message);
-       return res.redirect('/404');
-    }
-
-    res.redirect('/');
+        res.redirect('/404');
+    } 
 });
 
 export default authController;
