95b1fd73d6710db373ecd07b5e6692ba44a3b5e3
Georgi Donchev <georgeuk26@gmail.com>
2025-02-01 17:30:17 +0000

    Add dynamic navigation

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -30,7 +30,8 @@ movieController.post('/create', async (req, res) => {
 movieController.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOneWithCasts(movieId);
-    const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
+    // const isCreator = movie.creator && movie.creator.toString() === req.user?.id;
+    const isCreator = movie.creator?.equals(req.user?.id);
 
     res.render('movie/details', { movie, isCreator });
 });
