65c19ba97aaf333d7c4a0a34cf2c259965bbc680
Georgi Donchev <georgeuk26@gmail.com>
2025-02-01 18:38:06 +0000

    Show dynamic data in edit form

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -66,7 +66,11 @@ movieController.get('/:movieId/delete', async (req, res) => {
     res.redirect('/');
 });
 
-movieController.get('/:movieId/edit', (req, res) => {
-    res.render('movie/edit'); 
+movieController.get('/:movieId/edit', async (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = await movieService.getOne(movieId);
+
+    res.render('movie/edit', { movie });
 });
+
 export default movieController;
