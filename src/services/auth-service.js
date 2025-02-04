0d0698d3f41a6f50f0da563d4f2a2848a12ce042
Georgi Donchev <georgeuk26@gmail.com>
2025-02-04 23:30:12 +0000

    Check repeat password

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -5,7 +5,17 @@ import jwt from "jsonwebtoken";
 const SECRET = process.env.JWT_SECRET || 'BASICSECRET';
 
 export default {
-    register(userData) {
+    async register(userData) {
+        // Check if password match rePassword
+        // if (userData.password !== userData.rePassword) {
+        //     throw new Error('Password mismatch!');
+        // }
+
+        // Validate if email exists
+        const userCount = await User.countDocuments({email: userData.email});
+        if (userCount > 0) {
+            throw new Error('This email alredy exists!');
+        }
         return User.create(userData);
     },
     async login(email, password) {
